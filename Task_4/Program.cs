b3c40d0b7e61176a511a8a9077eb373afbffe60c
Sergey Zavyalov <ser500vms@gmail.com>
2023-07-21 20:50:45 +0300

    need to check with seminar

---
+++ Task_4/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи.
+// Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+// Последовательность чисел Фибоначчи Fn = F(n-1) + F(n-2),
+
+void Fibonacci(int n)
+{
+    if (n <= 0)
+    {
+        Console.Write(" 0 ");
+    }
+    else
+    {
+        Console.Write("0, 1, ");
+    }
+    long a = 0;
+    long b = 1;
+    int count = n;
+
+    for (int i = 3; i <= n; i++)
+    {
+        long temp = a + b;
+        a = b;
+        b = temp;
+        Console.Write($"{b}, ");
+    }
+    count--;
+}
+
+Fibonacci(5);
+// int n = 5;
+// long fib = Fibonacci(n);
+// Console.WriteLine("n-е число Фибоначчи при n=" + n + " равно " + fib);
+
+
+
+
+
+
+
---
+++ Task_5/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 45: Напишите программу, которая будет создавать копию 
+// заданного массива с помощью поэлементного копирования.
+
+
+int[] ArrayCopy(int[] array)
+{
+    int[] arrayCopy = new int[array.Length];
+    for (int i = 0; i < arrayCopy.Length; i++)
+    {
+        arrayCopy[i] = array[i];
+    }
+    return arrayCopy;
+}
+
+
+int[] GenerateRandomArray(int size, int start, int finish)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(start, finish + 1);
+    }
+    return array;
+}
+
+
+int[] array = GenerateRandomArray(5, 1, 9);
+Console.Write($"[{String.Join(", ", array)}] -> ");
+int[] copyArray = ArrayCopy(array);
+Console.Write($"[{String.Join(", ", copyArray)}]");
